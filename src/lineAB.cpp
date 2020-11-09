8ed10603246b72638ac9e4d97988412aa1e8f8f2
Clement Lemaire <lemairec02@gmail.com>
2020-11-09 23:08:56 +0100

    New line operation

--- src/lineAB.cpp
+++ src/lineAB.cpp
@@ -1,6 +1,7 @@
 #include "lineAB.hpp"
 #include <math.h>
 #include "logging.hpp"
+#include "util/util.hpp"
 
 double LineAB::distance(double x, double y, double lg){
     if(m_pointA.m_x!=0 && m_pointB.m_x!=0){
@@ -38,44 +39,26 @@ double LineAB::anglefollowTheCarrot(double x, double y, double lg, double angleD
     
     double angle_followKarott =  atan(distance/lk);
     double angle = angle_followKarott + angleABDeplacement;
+    angle = angleBetweenPI2(angle);
     
-    
-    if(angle>3.14/2){
-        angle = angle-3.14;
-    }
-    if(angle>3.14/2){
-        angle = angle-3.14;
-    }
-    if(angle < -3.14/2){
-        angle = angle+3.14;
-    }
-    if(angle < -3.14/2){
-        angle = angle+3.14;
-    }
     return angle;
 }
 
 double LineAB::calculRearWheelPosition(double p_x, double p_y, double lg, double deplacementAngle, double deplacement_x, double deplacement_y, double vitesse, double L, double KTH, double KE){
+    double x_segment = m_pointB.m_x - m_pointA.m_x;
+    double y_segment = m_pointB.m_y - m_pointA.m_y;
     
-    double angleABDeplacement = m_angleAB - deplacementAngle;
-    if(angleABDeplacement>3.14/2){
-        angleABDeplacement = angleABDeplacement-3.14;
-    }
-    if(angleABDeplacement < -3.14/2){
-        angleABDeplacement = angleABDeplacement+3.14;
-    }
+    double angle = -my_angle(deplacement_x, deplacement_y, x_segment, y_segment);
+    angle = angleBetweenPI2(angle);
     
     double distance = this->distance(p_x, p_y, lg);
-    
     
     double e = -distance;
     double v = vitesse*10000.0/3600.0;
     double k = 0;//todo;
-    double th_e = -angleABDeplacement;//todo;
+    double th_e = -angle;//todo;
 
     double omega = v * k * cos(th_e) / (1.0 - k * e) - KTH * abs(v) * th_e - KE * v * sin(th_e) * e / th_e;
-    
-    
     
     if (th_e == 0.0 || omega == 0.0){
         return 0.0;
---
+++ src/util/util.cpp
@@ -0,0 +1,44 @@
+#include "util.hpp"
+
+#include <math.h>
+
+double my_angle(double x1, double y1, double x2, double y2){
+    if(x1 == 0 && y1 ==0){
+        return 0;
+    }
+    double n1 = sqrt(x1*x1+y1*y1);
+    double n2 = sqrt(x2*x2+y2*y2);
+    double cosa = (x1*x2+y1*y2)/(n1*n2);
+    double siga = (x1*y2-y1*x2);
+    
+    double angle = acos(cosa);
+    if(siga < 0){
+        angle = -angle;
+    }
+    
+    
+    return angle;
+}
+
+double angleBetweenPI2(double a){
+    double angle = a;
+    if(a>3.14/2){
+        angle = angle-3.14;
+    }
+    if(angle>3.14/2){
+        angle = angle-3.14;
+    }
+    if(angle < -3.14/2){
+        angle = angle+3.14;
+    }
+    if(angle < -3.14/2){
+        angle = angle+3.14;
+    }
+    return angle;
+}
+
+bool isEqualDoubles2 (double a, double b, double epsilon)
+{
+   double diff = a - b;
+   return (diff > epsilon) || (-diff < -epsilon);
+}
