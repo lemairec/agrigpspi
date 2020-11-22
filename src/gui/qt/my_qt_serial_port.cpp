e28cd7e1bc8c4c54367acf01b7c10f5ba1169a4d
Clement Lemaire <lemairec02@gmail.com>
2020-11-22 16:26:05 +0100

    Hexa

--- src/gui/qt/my_qt_serial_port.cpp
+++ src/gui/qt/my_qt_serial_port.cpp
@@ -179,7 +179,7 @@ void MyQTSerialPorts::handleReadyReadImu(){
     
     for(int i = 0; i < b.size(); ++i){
         int j = data[i];
-        std::cout << j << " ";
+        std::cout << std::hex << j << " ";
     }
     std::cout << std::endl;
     DEBUG("end");
