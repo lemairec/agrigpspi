4b42d6a0676a9ad9fd30c1f46cc3c641171b158e
Clement Lemaire <lemairec02@gmail.com>
2020-11-22 17:25:14 +0100

    Parse

--- src/gui/qt/my_qt_serial_port.cpp
+++ src/gui/qt/my_qt_serial_port.cpp
@@ -179,6 +179,7 @@ void MyQTSerialPorts::handleReadyReadImu(){
     GpsFramework & f = GpsFramework::Instance();
     for(int i = 0; i < b.size(); ++i){
         int j = data[i];
+        f.m_imuModule.addUChar(j);
         std::cout << std::hex << j << " ";
     }
     std::cout << std::endl;
