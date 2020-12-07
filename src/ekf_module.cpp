462ac037704dbf7e400ddcf7a9bb7464a5642c27
Clement Lemaire <lemairec02@gmail.com>
2020-12-07 18:42:40 +0100

    Ekf

--- src/ekf_module.cpp
+++ src/ekf_module.cpp
@@ -1,15 +1,17 @@
 #include "ekf_module.hpp"
+
+#include "logging.hpp"
 #include <math.h>
 
 void EkfModule::onNewPoint(double x, double y, double z, double v, double ang, double ax, double ay, double az){
+    double dt = 0.1;
+    double x_v = m_old_x + sin(ang)*v*dt;
+    double y_v = m_old_y + cos(ang)*v*dt;
+    
     if(ax > 0){
-        double dt = 0.1;
         double x_acc = m_old_x + ax*dt*dt/2.0;
         double y_acc = m_old_y + ay*dt*dt/2.0;
         double z_acc = m_old_z + az*dt*dt/2.0;
-        
-        double x_v = m_old_x + cos(ang)*v;
-        double y_v = m_old_y + sin(ang)*v;
         
         double new_x = (x+x_acc+x_v)/3.0;
         double new_y = (y+y_acc+y_v)/3.0;
@@ -18,9 +20,13 @@ void EkfModule::onNewPoint(double x, double y, double z, double v, double ang, d
         m_old_x = new_x;
         m_old_y = new_y;
         m_old_z = new_z;
-    } else {
-        m_old_x = x;
-        m_old_y = y;
-        m_old_z = z;
+    }  else {
+        double new_x = (x+x_v)/2.0;
+        double new_y = (y+y_v)/2.0;
+        double new_z = z;
+        
+        m_old_x = new_x;
+        m_old_y = new_y;
+        m_old_z = new_z;
     }
 }
