ac7d3dbfaad215b79c6edc9ffd65999676d040fb
Clement Lemaire <lemairec02@gmail.com>
2020-12-16 15:17:43 +0100

    Fk essieu_arriere_y

--- src/gps_framework.cpp
+++ src/gps_framework.cpp
@@ -316,7 +316,7 @@ void GpsFramework::processPilot(double deplacementX, double deplacementY
             setDistance(dist);
             
             if(m_pilot_algo == AlgoPilot::FollowCarrot){
-                m_angle_correction = m_lineAB.anglefollowTheCarrot(essieu_avant_x, essieu_avant_y, m_deplacementX, m_deplacementY, m_pilot_lookahead_d);
+                m_angle_correction = m_lineAB.anglefollowTheCarrot(essieu_arriere_x, essieu_arriere_y, m_deplacementX, m_deplacementY, m_pilot_lookahead_d);
             } else if(m_pilot_algo == AlgoPilot::RearWheelPosition){
                 m_angle_correction = m_lineAB.calculRearWheelPosition(essieu_arriere_x, essieu_arriere_y, m_deplacementAngle, m_deplacementX, m_deplacementY, m_vitesse, 1.5, m_pilot_rwp_kth, m_pilot_rwp_kte);
             } else if(m_pilot_algo == AlgoPilot::RWPAndFC){
