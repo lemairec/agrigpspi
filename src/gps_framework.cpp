6d21f32fe128bfd17656f982e0b040c48800411b
lemaire <lemaire@abvent.fr>
2021-07-14 17:52:46 +0200

    Inverse

--- src/gps_framework.cpp
+++ src/gps_framework.cpp
@@ -395,7 +395,7 @@ void GpsFramework::processPilot(double deplacementX, double deplacementY
     }
     
     if(m_config.m_algo_volant_mode == AlgoVolantMode_CapteurVitesse){
-        double correction = m_angle_correction + m_config.m_agressivite_yawl*m_imuModule.m_w_z;
+        double correction = m_angle_correction - m_config.m_agressivite_yawl*m_imuModule.m_w_z;
         m_pilotModule.run(correction, m_time_last_point);
     } else {
         m_pilotModule.run(m_angle_correction, m_time_last_point);
